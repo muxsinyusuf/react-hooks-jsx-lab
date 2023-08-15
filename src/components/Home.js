bbb9c8a96731ce96067e258cb9851214c40d41ab
muxsinyusuf <mucsin.yusuf@student.moringaschool.com>
2023-08-15 20:10:35 +0300

    lab done

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,17 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  const textStyle = {
+    color: 'firebrick'
+  };
+  return <div id="home">
+    <h1 style={textStyle}>
+      {name} is a Web Developer from {city}
+      </h1>
+  
+    <city></city>
+  </div>;
+
 }
 
 export default Home;
